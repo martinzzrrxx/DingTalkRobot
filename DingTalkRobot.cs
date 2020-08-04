2bca652ed54c5878646efe23f92c10f8fbe0d3da
Martin <252951598@qq.com>
2020-08-04 11:16:55 +0800

    第二版

---
+++ DingTalkRobot.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+
+namespace DingTalkRobot {
+
+  // ref: https://ding-doc.dingtalk.com/doc#/serverapi2/qf2nxq
+  
+  public class DingTalkRobot {
+    private static readonly DateTime EPOCH = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+    private const string SUCCESS_RESPONSE = "{\"errcode\":0,\"errmsg\":\"ok\"}";
+
+    private string m_WebHookURL = "";
+    private string m_SecretKey = "";
+
+    public DingTalkRobot(String url, String secrete) {
+      m_WebHookURL = url;
+      m_SecretKey = secrete;
+    }
+    public void SendText(String text, params String[] ats) {
+      String arg0 = "";
+      String arg2 = "";
+      for (int i = 0; i < ats.Length; i++) {
+        if (i > 0) { arg0 += " "; arg2 += ","; }
+        arg0 += "@" + ats[i];
+        arg2 += ats[i];
+      }
+
+      SendRaw(String.Format(@"{{
+          ""msgtype"": ""text"", 
+          ""text"": {{
+              ""content"": ""{0} {1}""
+          }},
+          ""at"": {{
+              ""atMobiles"": [{2}], 
+              ""isAtAll"": {3}
+          }}
+        }}", arg0, text, arg2, arg0.Length > 0 ? "false" : "true"));
+
+    }
+    public void SendMarkDown(String title, string text, params String[] ats) {
+      String arg1 = "";
+      String arg3 = "";
+      for (int i = 0; i < ats.Length; i++) {
+        if (i > 0) { arg1 += ","; arg3 += ","; }
+        arg1 += "@" + ats[i];
+        arg3 += ats[i];
+      }
+
+      SendRaw(String.Format(@"{{
+            ""msgtype"": ""markdown"",
+            ""markdown"": {{
+                ""title"":""{0}"",
+                ""text"": ""{1} {2}""
+            }},
+            ""at"": {{
+                ""atMobiles"": [{3}],
+                ""isAtAll"": {4}
+            }}
+        }}", title, arg1, text, arg3, arg1.Length > 0 ? "false" : "true"));
+    }
+    public void SendRaw(String json) {
+      json = json.Replace("\r\n", "\n");
+      long timestamp = (long)(DateTime.UtcNow - EPOCH).TotalMilliseconds;
+      string signature = "";
+      using (HMACSHA256 hmac = new HMACSHA256(System.Text.Encoding.Default.GetBytes(m_SecretKey))) {
+        byte[] hashValue = hmac.ComputeHash(System.Text.Encoding.Default.GetBytes(timestamp + "\n" + m_SecretKey));
+
+        signature = Uri.EscapeDataString(Convert.ToBase64String(hashValue));
+      }
+
+      WebRequest request = WebRequest.Create(m_WebHookURL + "&timestamp=" + timestamp + "&sign=" + signature);
+      request.Method = "POST";
+      request.ContentType = "application/json; charset=utf-8";
+      request.Timeout = 60 * 1000;
+
+      byte[] bytes = System.Text.Encoding.UTF8.GetBytes(json);
+      using (Stream stream = request.GetRequestStream()) {
+        stream.Write(bytes, 0, bytes.Length);
+      }
+
+      WebResponse response = request.GetResponse();
+      using (Stream stream = response.GetResponseStream()) {
+        StreamReader reader = new StreamReader(stream);
+        string result = reader.ReadToEnd();
+
+        if (result.Equals(SUCCESS_RESPONSE)) {
+          Console.WriteLine("Send DingTalk Robot Message Successfully");
+        } else {
+          throw new Exception(result);
+        }
+      }
+    }
+
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,360 +1,201 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Net;
-using System.Runtime.Remoting.Messaging;
-using System.Security.Cryptography;
 using System.Text;
 
-namespace dingtalk {
+namespace DingTalkRobot {
   class Program {
 
-    private const string DEFAULT_INI_PATH = "Robot.ini";
+    private static string m_WebHookURL    = "";
+    private static string m_SecretKey     = "";
+    private static string m_TextMsg       = "";
+    private static string m_MarkdownTitle = "";
+    private static string m_MarkdownMsg   = "";
+    private static string m_Json          = "";
+    private static string m_NReport       = "";
+    private static string[] m_Ats         = new string[0];
+    private static Dictionary<string, string> m_ProdOwners = new Dictionary<string, string>();
 
     static void Main(string[] args) {
 
-      string configFile = "";
       try {
         string exePath = System.Reflection.Assembly.GetEntryAssembly().Location;
         string dir = new FileInfo(exePath).DirectoryName;
-        configFile = Path.Combine(dir, DEFAULT_INI_PATH);
+        IniLoad(Path.Combine(dir, "config.ini"));
       } catch (Exception ex) {
-        Console.WriteLine("Default config file " + DEFAULT_INI_PATH + " doesn't find");
-        configFile = "";  
+        Console.WriteLine("Default config file config.ini doesn't find");
       }
-
-      string   webHookURL      = "";
-      string   secretKey       = "";
-      string   textMsg         = "";
-      string   markdownMsg     = "";
-      string   json            = "";
-      string   dailyBuildMsg   = "";
-      string[] mobiles         = new string[0];
 
       int i = 0;
       while (i < args.Length) {
 
         switch (args[i].ToLower()) {
           case "-f":
-            configFile = args[++i];
+            IniLoad( args[++i]);
             break;
           case "-u":
-            webHookURL = args[++i];
+            m_WebHookURL = args[++i];
             break;
           case "-s":
-            secretKey = args[++i];
+            m_SecretKey = args[++i];
             break;
           case "-t":
-            textMsg = args[++i];
+            m_TextMsg = args[++i];
             break;
           case "-m":
-            markdownMsg = args[++i];
+            m_MarkdownMsg = args[++i];
             break;
-          case "-a":
-            mobiles = Utils.Split(args[++i], ",");
+          case "-mt":
+            m_MarkdownTitle = args[++i];
             break;
           case "-j":
-            json = args[i++];
+            m_Json = args[++i];
             break;
-          case "-d":
-            dailyBuildMsg = args[i++];
+          case "-a":
+            ParseAt(args[++i]);
+            break;
+          case "-n":
+            m_NReport = args[++i];
             break;
           default:
-            throw new Exception("Unknown arguments: \"" + args[i] + "\"");
+            throw new Exception("Unknown arguments: \"" + args[i] + "\" " + args[++i]);
         }
         i++;
       }
+      if (m_WebHookURL.Length == 0) throw new Exception("WebHookUrl is required");
+      if (m_SecretKey.Length == 0) throw new Exception("SecretKey is required");
+      if (m_TextMsg.Length == 0 && m_MarkdownMsg.Length == 0 && m_Json.Length == 0 && m_NReport.Length == 0) 
+        throw new Exception("Markdown/Text/Json/NReport is required");
 
-      try {
-        Config config = Config.LoadConfig(configFile);
 
-        if (webHookURL.Length > 0) config.WebHookUrl = webHookURL;
-        if (secretKey.Length > 0) config.SecretKey = secretKey;
-        if (textMsg.Length > 0) config.TextMsg = textMsg;
-        if (markdownMsg.Length > 0) config.MarkdownMsg = markdownMsg;
-        if (json.Length > 0) config.Json = json;
-        if (mobiles.Length > 0) config.Mobiles = mobiles;
-
-        config.Validate();
-
-        Robot robot = new Robot(config);
-        robot.Send();
-      } catch (Exception ex) {
-        // ignore all exception
-        Console.WriteLine("DING TALK ROBOT ERROR: " + ex.Message + " \n stacktrace: " + ex.StackTrace);
+      DingTalkRobot robot = new DingTalkRobot(m_WebHookURL, m_SecretKey);
+      if (m_NReport.Length > 0) {
+        try {
+          List<string> msgs = FormatNSoftwareReport(m_NReport);
+          for (i = 0; i < msgs.Count; i+=2) {
+            robot.SendText(msgs[i] + "\n\n" + msgs[i+1], new string[] { GetProdOwner(msgs[i]), "15399015948" });
+          }
+        } catch (Exception ex) {
+          Console.WriteLine("Failed to send NReport: " + ex.Message);
+        }
+      } 
+      if (m_Json.Length > 0) {
+        try {
+          robot.SendRaw(m_Json);
+        } catch (Exception ex) {
+          Console.WriteLine("Failed to send JSON: " + ex.Message);
+        }
+      } 
+      if (m_MarkdownMsg.Length > 0) {
+        try {
+          robot.SendMarkDown(m_MarkdownTitle, m_MarkdownMsg, m_Ats);
+        } catch (Exception ex) {
+          Console.WriteLine("Failed to send Markdown: " + ex.Message);
+        }
+      }
+      if (m_TextMsg.Length > 0) {
+        try {
+          robot.SendText(m_TextMsg, m_Ats);
+        } catch (Exception ex) {
+          Console.WriteLine("Failed to send Text: " + ex.Message);
+        }
       }
     }
 
-    class Config {
+    public static void IniLoad(string file) {
 
-      public string WebHookUrl = "";
-      public string SecretKey = "";
-      public string TextMsg = "";
-      public string MarkdownMsg = "";
-      public string Json = ""; // file or json data
-      public string[] Mobiles = new string[0];
+      string[] lines = File.ReadAllLines(file);
+      for (int i = 0; i < lines.Length; i++) {
+        if (lines[i].Contains("=")) {
+          int pos = lines[i].IndexOf("=");
+          string key = lines[i].Substring(0, pos).Trim();
+          string val = lines[i].Substring(pos + 1).Trim();
 
-      public Dictionary<string, Dictionary<string, string>> Sections = new Dictionary<string, Dictionary<string, string>>();
-
-      public static Config LoadConfig(string path) {
-        string[] lines = File.ReadAllLines(path);
-        if (lines == null || lines.Length == 0)
-          throw new Exception("Invalid config file: " + path);
-
-        string section = ""; // if the section is not empty, we are parsing the SECTION
-        Config config = new Config();
-        for (int i = 0; i < lines.Length; i++) {
-          string item = lines[i].Trim();
-          if (item.Length == 0 || item.StartsWith(";")) continue;
-
-          if (item.StartsWith("[") && item.EndsWith("]")) {
-            section = item.Substring(1, item.Length - 2);
-            if (section.Length == 0 || section.Trim().Length == 0) throw new Exception("Section name cannot be empty");
-            if (config.Sections.ContainsKey(section)) throw new Exception("Duplicate section \"" + section + "\"");
-            config.Sections.Add(section, new Dictionary<string, string>());
-            continue;
-          }
-
-          int idx = item.IndexOf("=");
-          if (idx <= 0) throw new Exception("Invalid config entry: \"" + lines[i] + "\"");
-
-          string[] kv = new string[2];
-          kv[0] = item.Substring(0, idx).ToUpper();
-          kv[1] = item.Substring(idx + 1);
-
-
-          if (section.Length > 0) {
-            config.Sections[section].Add(kv[0], kv[1]);
-            continue;
-          }
-
-          switch (kv[0]) {
+          switch (key.ToUpper()) {
             case "WEBHOOKURL":
-              config.WebHookUrl = kv[1];
+              m_WebHookURL = val;
               break;
             case "SECRETKEY":
-              config.SecretKey = kv[1];
-              break;
-            case "TEXT":
-              config.TextMsg = kv[1];
-              break;
-            case "MARKDOWN":
-              config.MarkdownMsg = kv[1];
-              break;
-            case "JSON":
-              config.Json = kv[1];
-              break;
-            case "MOBILES":
-              config.Mobiles = Utils.Split(kv[1], ",");
+              m_SecretKey = val;
               break;
             default:
-              Console.WriteLine("Unknow config entry: \"" + lines[i] + "\"");
+              if (IsPhoneNum(key)) {
+                String[] vals = val.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                for (int j = 0; j < vals.Length; j++) {
+                  vals[j] = vals[j].Trim();
+                  m_ProdOwners.Add(vals[j], key);
+                }
+              } else {
+                Console.WriteLine("Ignore invalid config \"" + key + "\"");
+              } 
               break;
           }
         }
-
-        return config;
-      }
-      public void Validate() {
-        if (WebHookUrl.Length == 0) throw new Exception("WebHookUrl is required");
-        if (SecretKey.Length == 0) throw new Exception("SecretKey is required");
-        if (TextMsg.Length == 0 && MarkdownMsg.Length == 0 && Json.Length == 0 && DialyBuildMsg.Length == 0) throw new Exception("Markdown/Text/Json/DialyBuildMsg is required");
-        // if Mobiles is null, we will at all people
-      }
-
-    }
-
-    // ref: https://ding-doc.dingtalk.com/doc#/serverapi2/qf2nxq
-
-    class Robot {
-      private static readonly DateTime EPOCH = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-
-
-      private const string SUCCESS_RESPONSE = "{\"errcode\":0,\"errmsg\":\"ok\"}";
-
-      private const string TEXT_MSG = @"
-{
-  ""msgtype"": ""text"", 
-  ""text"": {
-      ""content"": ""$TEXT$""
-  }, 
-  ""at"": {
-      ""atMobiles"": [
-          $MOBILES$
-      ], 
-      ""isAtAll"": $ISATALL$
-  }
-}
-";
-      private const string MARKDOWN_MSG = @"
-{
-     ""msgtype"": ""markdown"",
-     ""markdown"": {
-         ""title"":""$TITLE$"",
-         ""text"": ""$MARKDOWN$\n""
-     },
-     ""at"": {
-          ""atMobiles"": [
-              $MOBILES$
-          ],
-          ""isAtAll"": $ISATALL$
-      }
-  }
-
-";
-
-      private Config m_Config = null;
-
-      public Robot(Config config) {
-        m_Config = config;
-      }
-
-      public void Send() {
-        string json = BuildJSONMsg();
-        long timestamp = CurrentTimeMillis();
-        string signature = Sign(timestamp);
-
-        WebRequest request = WebRequest.Create(m_Config.WebHookUrl + "&timestamp=" + timestamp + "&sign=" + signature);
-        request.Method = "POST";
-        request.ContentType = "application/json; charset=utf-8";
-        request.Timeout = 60 * 1000;
-
-        byte[] bytes = System.Text.Encoding.UTF8.GetBytes(json);
-        using (Stream stream = request.GetRequestStream()) {
-          stream.Write(bytes, 0, bytes.Length);
-        }
-
-        WebResponse response = request.GetResponse();
-        using (Stream stream = response.GetResponseStream()) {
-          StreamReader reader = new StreamReader(stream);
-          string result = reader.ReadToEnd();
-
-          if (result.Equals(SUCCESS_RESPONSE)) {
-            Console.WriteLine("Send DingTalk Robot Message Successfully");
-          } else {
-            throw new Exception(result);
-          }
-        }
-      }
-
-      private string GetMsgReceiver(string msg) {
-        if (m_Config.Sections.ContainsKey("Subscription")) {
-          foreach (KeyValuePair<string, string> entry in m_Config.Sections["Subscription"]) {
-            string[] keywords = Utils.Split(entry.Value, ",");
-            for (int i = 0; i < keywords.Length; i++) {
-              if (msg.StartsWith(keywords[i]) && msg[keywords[i].Length] == ' ')
-                return entry.Key;
-            }
-          }
-        }
-        return "";
-      }
-
-      private string BuildJSONMsg() {
-
-        if (m_Config.TextMsg.Length > 0) {
-          string receiver = GetMsgReceiver(m_Config.TextMsg);
-          string msg = TEXT_MSG;
-          msg = msg.Replace("$TEXT$", receiver.Length > 0 ? ("@" + receiver + "\n" + m_Config.TextMsg) : m_Config.TextMsg);
-
-          string mobiles = "";
-          for (int i = 0; i < m_Config.Mobiles.Length; i++) {
-            mobiles += m_Config.Mobiles[i];
-            if (i < m_Config.Mobiles.Length - 1) mobiles += ",";
-            mobiles += "\n";
-          }
-          msg = msg.Replace("$MOBILES$", mobiles);
-          msg = msg.Replace("$ISATALL$", mobiles.Length > 0 ? "false" : "true");
-          return msg;
-        } else if (m_Config.MarkdownMsg.Length > 0) {
-          string msg = MARKDOWN_MSG;
-          msg = msg.Replace("$TITLE$", "TITLE"); // just hard code the title for now
-          msg = msg.Replace("$MARKDOWN$", m_Config.MarkdownMsg);
-
-          string mobiles = "";
-          for (int i = 0; i < m_Config.Mobiles.Length; i++) {
-            mobiles += m_Config.Mobiles[i];
-            if (i < m_Config.Mobiles.Length - 1) mobiles += ",";
-            mobiles += "\n";
-          }
-          msg = msg.Replace("$MOBILES$", mobiles);
-          msg = msg.Replace("$ISATALL$", mobiles.Length > 0 ? "false" : "true");
-          return msg;
-        } else if (m_Config.Json.Length > 0) {
-          if (File.Exists(m_Config.Json)) {
-            return File.ReadAllText(m_Config.Json);
-          }
-          return m_Config.Json;
-        } else {
-          // Should never reach here
-        }
-        return "";
-      }
-
-
-      private string Sign(long timestamp) {
-        using (HMACSHA256 hmac = new HMACSHA256(System.Text.Encoding.Default.GetBytes(m_Config.SecretKey))) {
-          byte[] hashValue = hmac.ComputeHash(System.Text.Encoding.Default.GetBytes(timestamp + "\n" + m_Config.SecretKey));
-
-          return Uri.EscapeDataString(System.Convert.ToBase64String(hashValue));
-        }
-      }
-      private long CurrentTimeMillis() {
-        return (long)(DateTime.UtcNow - EPOCH).TotalMilliseconds;
-      }
-      
-      private List<String> FormatNSoftwareReport(String m) {
-        List<String> ret = new List<string>();
-        int pos = m.IndexOf("<table");
-        int posEnd = m.IndexOf(("</table>"));
-        while (pos < posEnd) {
-          int rowStart = m.IndexOf("<tr>", pos);
-          int rowEnd = m.IndexOf("</tr>", pos);
-          pos = rowEnd + "</tr>".Length;
-
-          String rowStr = m.Substring(rowStart + "<tr>".Length, rowEnd - rowStart - "<tr>".Length);
-          rowStr = rowStr.Replace("</td>", "");
-          String[] cols = rowStr.Split(new string[] { "<td>" }, StringSplitOptions.RemoveEmptyEntries);
-          if (cols.Length != 2) continue;
-          cols[0] = RemoveTag(cols[0]);
-          cols[1] = RemoveTag(cols[1].Replace("<br>", "\r\n"));
-          pos = cols[1].IndexOf("Microsoft (R) Program");
-          if (pos > 0) cols[1] = cols[1].Substring(pos);
-          while (cols[1].Contains("\r\n\r\n")) {
-            cols[1] = cols[1].Replace("\r\n\r\n", "\r\n");
-          }
-          ret.AddRange(cols);
-        }
-        return ret;
-      }
-      private String RemoveTag(String input) {
-        StringBuilder sb = new StringBuilder();
-        bool inTag = false;
-        for (int i = 0; i < input.Length; i++) {
-          char ch = input[i];
-          if (ch == '<')               { inTag = true;  continue; } 
-          else if (inTag && ch == '>') { inTag = false; continue; }
-          if (!inTag) sb.Append(ch);
-        }
-        return sb.ToString();
       }
     }
-
-    class Utils {
-      public static string[] Split(string str, string delimiter) {
-        if (str == null || str.Trim().Length == 0) return new string[0];
-
-        List<string> ret = new List<string>();
-        string[] parts = str.Split(new string[] { delimiter }, StringSplitOptions.RemoveEmptyEntries);
-        for (int i = 0; i < parts.Length; i++) {
-          ret.Add(parts[i].Trim());
-        }
-        return ret.ToArray();
+    private static bool IsPhoneNum(String num) {
+      num = num.Trim();
+      if (num.Length != 11) return false;
+      if (num[0] != '1') return false;
+      for (int i = 0; i < num.Length; i++) {
+        if (num[i] >= '0' && num[i] <= '9') continue;
+        return false;
       }
+      return true;
+    }
+
+    private static void ParseAt(String ats) {
+      List<String> ret = new List<String>();
+      String[] vals = ats.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+      for (int j = 0; j < vals.Length; j++) {
+        vals[j] = vals[j].Trim();
+        if (IsPhoneNum(vals[j])) ret.Add(vals[j]);
+      }
+      m_Ats = ret.ToArray();
+    }
+    
+    private static List<String> FormatNSoftwareReport(String m) {
+      if (File.Exists(m)) m = File.ReadAllText(m);
+      List<String> ret = new List<string>();
+      int pos = m.IndexOf("<table");
+      int posEnd = m.IndexOf(("</table>"));
+      while (pos < posEnd) {
+        int rowStart = m.IndexOf("<tr>", pos);
+        int rowEnd = m.IndexOf("</tr>", pos);
+        pos = rowEnd + "</tr>".Length;
+
+        String rowStr = m.Substring(rowStart + "<tr>".Length, rowEnd - rowStart - "<tr>".Length);
+        rowStr = rowStr.Replace("</td>", "");
+        String[] cols = rowStr.Split(new string[] { "<td>" }, StringSplitOptions.RemoveEmptyEntries);
+        if (cols.Length != 2 || cols[1].Equals("Success!")) continue;
+        cols[0] = RemoveTag(cols[0]);
+        cols[1] = RemoveTag(cols[1].Replace("<br>", "\n"));
+        cols[1] = cols[1].Replace("\r\n", "\n");
+        pos = cols[1].IndexOf("Microsoft (R) Program");
+        if (pos > 0) cols[1] = cols[1].Substring(pos);
+        while (cols[1].Contains("\n\n")) {
+          cols[1] = cols[1].Replace("\n\n", "\n");
+        }
+        cols[1] = cols[1].Replace("\\", "\\\\").Replace("\"", "\\\"");
+        ret.AddRange(cols);
+      }
+      return ret;
+    }
+    private static String RemoveTag(String input) {
+      StringBuilder sb = new StringBuilder();
+      bool inTag = false;
+      for (int i = 0; i < input.Length; i++) {
+        char ch = input[i];
+        if (ch == '<') { inTag = true; continue; } else if (inTag && ch == '>') { inTag = false; continue; }
+        if (!inTag) sb.Append(ch);
+      }
+      return sb.ToString();
+    }
+
+    private static String GetProdOwner(String prod) {
+      int pos = prod.IndexOf("[");
+      string prodName = prod.Substring(0, pos).Trim();
+      return m_ProdOwners[prodName];
     }
   }
-
-
-
-
 }
